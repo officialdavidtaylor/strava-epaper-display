4f4c9e7a5bdf0956b2c958957e386e53b3dde337
David Taylor <officialdavidtaylor@gmail.com>
2022-02-28 21:05:35 -0800

    comit empty strava-credentials file

---
+++ strava-credentials.py
@@ -0,0 +1,9 @@
+# Store your Strava Credentials in this file, rest assured this file is
+# hidden via the .gitignore file
+
+StravaCredentials = {
+    'client_id': '',
+    'client_secret': '',
+    'refresh_token': '',
+    'access_token': ''
+}
