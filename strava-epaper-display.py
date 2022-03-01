95682ee32e51c89aa9b9f1b40805492d5ea8abd8
David Taylor <officialdavidtaylor@gmail.com>
2022-03-01 11:01:48 -0800

    mapbox api integration, export png w/ overlay

--- strava-epaper-display.py
+++ strava-epaper-display.py
@@ -6,10 +6,11 @@
 
 # --Libraries--
 
-from asyncio.windows_events import NULL
 import credentials as auth
 import requests
 import urllib3
+import urllib.parse
+import polyline
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # --Functions--
@@ -29,9 +30,9 @@ def GetRecentStravaData():
     }
 
     print('Requesting Token...\n')
-    response = requests.post(auth_url, data=payload, verify=False)
+    res = requests.post(auth_url, data=payload, verify=False)
     try:
-        access_token = response.json()['access_token']
+        access_token = res.json()['access_token']
         auth.StravaCredentials['acess_token'] = access_token
     except:
         print('POST request failed, new access token not generated')
@@ -39,28 +40,65 @@ def GetRecentStravaData():
     print('Access token retrieved and updated')
 
     try:
+        # poll server for new activities
         header = {'Authorization': 'Bearer ' + access_token}
         strava_data = requests.get(activites_url, headers=header).json()
-        # if operation is successful, return data
-        return strava_data
+
+        # identify most recent ride and return it
+        for activity in strava_data:
+            if activity['type'] == 'Ride':
+                return activity
+
     except:
         print('GET request failed, please try again later')
         return False
 
-# def GetMapImage(route_polyline):
-#   # TODO: use Maps Static API to generate map image from Strava route polyline
-#     # code here
 
+def GetMapImage(route_polyline):
+    map_resolution = '300x300'
+    file_name = 'map.png'
+
+    # URI encode the polyline data
+    route_polyline = urllib.parse.quote(route_polyline.encode('utf8'))
+
+    # use Static Map API to generate map image from Strava route polyline
+    mapbox_url = 'https://api.mapbox.com/styles/v1/mapbox/light-v10/static/path+000000({0})/auto/{1}?access_token={2}'
+    # insert path and access token
+    mapbox_url = mapbox_url.format(route_polyline, map_resolution,
+                                   auth.MapboxCredentials['access_token'])
+
+    print(mapbox_url)
+
+    try:
+        h = requests.head(mapbox_url, allow_redirects=True)
+        header = h.headers
+        content_type = header.get('content-type')
+
+        print(content_type)
+
+        res = requests.get(mapbox_url, allow_redirects=True)
+        image = open(file_name, 'wb')
+        image.write(res.content)
+        image.close()
+        return file_name
+    except:
+        print('error retrieving image from the server')
 
 # def GenerateEpaperOutput():
 #   # TODO: use pillow to create an image from Google API call and Strava Data
 #     # more code here
 
+# def DisplayError():
+#   # TODO: Generate template error image in Gimp, save to GitHub so it can be displayed
+#   # TODO: Generate live QR code to redirect to docs for specific error
+
+
 # --Script Logic--
+strava_activity = GetRecentStravaData()
 
-strava_data = GetRecentStravaData()
+strava_polyline = strava_activity["map"]["summary_polyline"]
 
-# map_image = GetMapImage(strava_data[0]["map"]["summary_polyline"])
+map_image = GetMapImage(strava_polyline)
 
 # display_output = GenerateEpaperOutput(map_image, strava_data[0])
 
